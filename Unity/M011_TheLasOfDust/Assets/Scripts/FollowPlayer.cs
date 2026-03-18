7dd77a03be4ab11c4f98fc73cc254f01441b4c5e
Jeronimo Martinez <j3r1t0h@hotmail.es>
2026-03-18 15:51:20 +0100

    Prototip 2
    He afegit les accions de atacar i de correr

--- Unity/M011_TheLasOfDust/Assets/Scripts/FollowPlayer.cs
+++ Unity/M011_TheLasOfDust/Assets/Scripts/FollowPlayer.cs
@@ -10,13 +10,11 @@ public class FollowPlayer : MonoBehaviour
 
     Vector3 localRot;
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.position = Player.position;
--- Unity/M011_TheLasOfDust/Assets/Scripts/PlayerController.cs
+++ Unity/M011_TheLasOfDust/Assets/Scripts/PlayerController.cs
@@ -5,21 +5,28 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public static PlayerController instance;
-
+    //Movimiento
     public float moveSpeed;
+    public float sprintSpeed;
+    private float currentSpeed;
     public float jumpForce;
     public float gravityScale = 5f;
-
+   
     private Vector3 moveDirection;
     public float VerticalVelocity => moveDirection.y;
-
+    //Componentes
     public CharacterController charController;
     public Camera playerCamera;
-
+    //Knockback
     public bool isKnocking;
     public float knocbackLength = .5f;
     private float knockbackCounter;
     public Vector2 knockbackPower;
+    //Tackle
+    public float tackleForce = 12f;
+    public float tackleDuration = 0.3f;
+    private bool isTackling;
+    private float tackleCounter;
 
     private void Awake()
     {
@@ -33,13 +40,30 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if(!isKnocking)
+        if (!isKnocking && !isTackling)
         {
+            //Tackle
+            if (Input.GetButtonDown("Fire1"))
+            {
+                StartTackle();
+            }
+
             float yStore = moveDirection.y;
 
             //Movimiento
             moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
-            moveDirection = moveDirection * moveSpeed;
+
+            //Sprint
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                currentSpeed = sprintSpeed;
+            }
+            else
+            {
+                currentSpeed = moveSpeed;
+            }
+
+            moveDirection = moveDirection * currentSpeed;
             moveDirection.y = yStore;
 
             //Salto
@@ -58,9 +82,10 @@ public class PlayerController : MonoBehaviour
                 Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
             }
         }
+
         if(isKnocking)
         {
-
+            
             knockbackCounter -= Time.deltaTime;
 
             moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
@@ -73,7 +98,23 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        
+        if (isTackling)
+        {
+            tackleCounter -= Time.deltaTime;
+
+            moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
+
+            charController.Move(moveDirection * Time.deltaTime);
+
+            if (tackleCounter <= 0)
+            {
+                isTackling = false;
+            }
+
+            return; 
+        }
+
+
     }
     
     public void Knockback(Vector3 enemyPosition)
@@ -95,5 +136,16 @@ public class PlayerController : MonoBehaviour
         moveDirection.y = jumpForce * 0.6f;
     }
 
-    
+    public void StartTackle()
+    {
+        isTackling = true;
+        tackleCounter = tackleDuration;
+
+        Vector3 forward = transform.forward;
+
+        moveDirection = forward * tackleForce;
+        moveDirection.y = 0f;
+    }
+
+
 }
